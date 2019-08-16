e05eb520f84b1c6e7e0a31ed826fbfbe7cce1337
jajoh143 <jjohnson@payitgov.com>
2019-08-15 20:15:24 -0500

    Updated api calls for resources

---
+++ azure-build/precache-manifest.ca0e4d8655761cd92cf1b69e424cb019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e0a7fc7b3e6209a868695fa30875f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53f7ee881699ddbf7329",
+    "url": "/static/css/2.7e95cfd5.chunk.css"
+  },
+  {
+    "revision": "057f43048b86a50c051b",
+    "url": "/static/css/main.c4002713.chunk.css"
+  },
+  {
+    "revision": "53f7ee881699ddbf7329",
+    "url": "/static/js/2.f7fcaf34.chunk.js"
+  },
+  {
+    "revision": "057f43048b86a50c051b",
+    "url": "/static/js/main.ea4dcbd7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
